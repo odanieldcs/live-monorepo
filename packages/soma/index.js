2806c3f11d772d3c0f69cd351d1d59a8243daabd
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2024-10-29 19:57:28 -0300

    feat: adicionado nova funcao de soma para 4 numeros

--- packages/soma/index.js
+++ packages/soma/index.js
@@ -4,7 +4,11 @@ const sum = (a, b) => a + b;
 // func'ão de soma de 3 números
 const sumWithThreeNumbers = (a, b, c) => a + b + c;
 
+// função de soma de 4 números
+const sumWithFourNumbers = (a, b, c, d) => a + b + c + d;
+
 module.exports = {
 	sum,
 	sumWithThreeNumbers,
+	sumWithFourNumbers,
 };
