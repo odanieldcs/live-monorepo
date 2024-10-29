f50b7a5da764d46d80e966c519f02fd332002ce7
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2024-10-29 19:48:06 -0300

    feat: adicionado nova função de soma

--- packages/soma/index.js
+++ packages/soma/index.js
@@ -1,4 +1,10 @@
 // função de soma de dois números
 const sum = (a, b) => a + b;
 
-module.exports = sum;
+// func'ão de soma de 3 números
+const sumWithThreeNumbers = (a, b, c) => a + b + c;
+
+module.exports = {
+	sum,
+	sumWithThreeNumbers,
+};
